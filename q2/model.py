32658b876afeba49cd8b050222a8932b35548108
Cagri Yonca <cagri.yonca@ceng.ktu.edu.tr>
2020-04-11 18:52:14 +0000

    last

--- q2/model.py
+++ q2/model.py
@@ -9,8 +9,8 @@ print("Preparing training data")
 X_train = []
 Y_train = []
 
-for i in range(100000):
-    for j in range(100000):
+for i in range(10000):
+    for j in range(1000):
         X_train.append([i * 0.01, j * 0.01])
         Y_train.append([i * j * 0.001])
 
@@ -18,8 +18,8 @@ print("Preparing test data")
 X_test = []
 Y_test = []
 
-for i in range(90000,110000):
-    for j in range(100000):
+for i in range(9000,11000):
+    for j in range(1000):
         X_test.append([i * 0.01, j * 0.01])
         Y_test.append([i * j * 0.001])
 
@@ -27,8 +27,8 @@ print("Preparing validation data")
 X_val = []
 Y_val = []
 
-for i in range(100000,120000):
-    for j in range(100000):
+for i in range(10000,12000):
+    for j in range(1000):
         X_val.append([i * 0.01, j * 0.01])
         Y_val.append([i * j * 0.01])
 
@@ -48,23 +48,28 @@ def create_model():
 
     return model
 
+checkpoint_path = "training_1/model.ckpt"
+
 def save_checkpoint():
     print("Saving model")
-    checkpoint_path = "deneme.ckpt"
+    checkpoint_path = checkpoint_path
     cp_callback = ModelCheckpoint(filepath=checkpoint_path,
-            save_weights_only=True)
+            save_weights_only=True,
+            verbose=1)
 
     return cp_callback
 
 is_load = 0
 model = create_model()
 if(is_load):
-    model.load_weights("deneme.cpt")
+    model.load_weights(training_path)
 else:
     cb_1 = save_checkpoint()
     print("Fitting model")
     model.fit(X_train, Y_train, epochs=10, batch_size=1,
-            validation_data=(X_val, Y_val))
+            validation_data=(X_val, Y_val),
+            callbacks=[cb_1])
+
     print("Evaluating model")
     test_loss, test_acc = model.evaluate(X_test, Y_test)
     print("Test acc: ", test_acc)
