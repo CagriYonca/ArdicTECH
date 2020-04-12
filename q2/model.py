74c97a1574fbf6b331bb04be1238a6e1e23bf617
Cagri Yonca <cagri.yonca@ceng.ktu.edu.tr>
2020-04-12 11:10:37 +0000

    last

--- q2/model.py
+++ q2/model.py
@@ -3,34 +3,36 @@ import numpy as np
 from tensorflow import keras
 from tensorflow.keras.callbacks import ModelCheckpoint
 
+is_load = 1
 
-# prepare training datas
-print("Preparing training data")
-X_train = []
-Y_train = []
+if is_load == 0:
+    # prepare training datas
+    print("Preparing training data")
+    X_train = []
+    Y_train = []
 
-for i in range(10000):
-    for j in range(1000):
-        X_train.append([i * 0.01, j * 0.01])
-        Y_train.append([i * j * 0.001])
+    for i in range(1000):
+        for j in range(1000):
+            X_train.append([i * 0.01, j * 0.01])
+            Y_train.append([i * j * 0.0001])
 
-print("Preparing test data")
-X_test = []
-Y_test = []
+    print("Preparing test data")
+    X_test = []
+    Y_test = []
 
-for i in range(9000,11000):
-    for j in range(1000):
-        X_test.append([i * 0.01, j * 0.01])
-        Y_test.append([i * j * 0.001])
+    for i in range(900,1100):
+        for j in range(1000):
+            X_test.append([i * 0.01, j * 0.01])
+            Y_test.append([i * j * 0.0001])
 
-print("Preparing validation data")
-X_val = []
-Y_val = []
+    print("Preparing validation data")
+    X_val = []
+    Y_val = []
 
-for i in range(10000,12000):
-    for j in range(1000):
-        X_val.append([i * 0.01, j * 0.01])
-        Y_val.append([i * j * 0.01])
+    for i in range(1000,1200):
+        for j in range(1000):
+            X_val.append([i * 0.01, j * 0.01])
+            Y_val.append([i * j * 0.0001])
 
 def create_model():
     print("Creating model")
@@ -52,17 +54,15 @@ checkpoint_path = "training_1/model.ckpt"
 
 def save_checkpoint():
     print("Saving model")
-    checkpoint_path = checkpoint_path
     cp_callback = ModelCheckpoint(filepath=checkpoint_path,
             save_weights_only=True,
             verbose=1)
 
     return cp_callback
 
-is_load = 0
 model = create_model()
 if(is_load):
-    model.load_weights(training_path)
+    model.load_weights(checkpoint_path)
 else:
     cb_1 = save_checkpoint()
     print("Fitting model")
@@ -74,5 +74,6 @@ else:
     test_loss, test_acc = model.evaluate(X_test, Y_test)
     print("Test acc: ", test_acc)
 
-a = np.array([[20,2], [10,9], [25, 4]])
+a = np.array([[10, 0.1], [0.2, 3], [4, 0.6], [0.5, 0.6]])
+print("a = np.array([[10, 0.1], [0.2, 3], [4, 0.6], [0.5, 0.6]])")
 print(model.predict(a))
